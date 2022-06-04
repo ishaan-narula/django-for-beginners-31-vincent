1cc8832c5bbf576a2185ad3d2b52c923671f75d4
ishaan-narula <60092550+ishaan-narula@users.noreply.github.com>
2022-06-04 22:36:38 +0530

    commits

--- django_for_beginners_31_ch5/blog/tests.py
+++ django_for_beginners_31_ch5/blog/tests.py
@@ -15,7 +15,7 @@ class BlogTest(TestCase):
 
         self.post = Post.objects.create(
             title='A good title',
-            body='Nice body content'
+            body='Nice body content',
             author=self.user
         )
 
@@ -25,4 +25,19 @@ class BlogTest(TestCase):
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
-        self.assertEqual
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice body content')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body content')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'Nice body content')
+        self.assertTemplateUsed(response, 'post_detail.html')
