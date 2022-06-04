d2f9a8cf8070a29b876dbc4f26a6881a26bccfa0
ishaan-narula <60092550+ishaan-narula@users.noreply.github.com>
2022-06-04 16:24:02 +0530

    commits

--- django_for_beginners_31_ch5/blog/views.py
+++ django_for_beginners_31_ch5/blog/views.py
@@ -3,6 +3,6 @@ from django.views.generic import ListView
 from .models import Post
 
 # Create your views here.
-class BlogView(ListView):
+class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
