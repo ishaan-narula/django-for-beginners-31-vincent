6113bf9dce406f7be61dbdf4fa42d6c277c1f20e
ishaan-narula <60092550+ishaan-narula@users.noreply.github.com>
2022-06-05 13:43:24 +0530

    commits

--- django_for_beginners_31_ch5-7/config/settings.py
+++ django_for_beginners_31_ch5-7/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog', # new
+    'accounts', # new
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
